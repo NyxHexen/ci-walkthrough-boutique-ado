541bf7623f433631e2b700c8c24910f5d471ccd0
NyxHexen <jd.davis948@gmail.com>
2023-02-01 20:31:57 +0000

    Added stripe elemnts

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MWmg8DKKUgEtkfSxPNrUSY2eMxdvII9yzSdL3bwBBF8XL4Xrri4eJ3jhpH35fxoRAd5vRp31R5SSdF7Bq9P4KMV00OiWmEPBm',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
