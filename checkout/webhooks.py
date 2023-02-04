72bac7e9bf5a41d4e0c77b57ea77b51e78c67d7b
NyxHexen <jd.davis948@gmail.com>
2023-02-04 13:28:24 +0000

    Added class methods to webhook handler and webhook view

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -36,7 +36,7 @@ def webhook(request):
         return HttpResponse(content=e, status=400)
 
     # Set up a webhook handler
-    handller = StripeWH_Handler(request)
+    handler = StripeWH_Handler(request)
 
     # Map webhook events to relevant handler functions
     event_map = {
